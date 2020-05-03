3521e8fcd95564fc3ed3781b804675a2cb00512d
jcbagtas <17738997+jcbagtas@users.noreply.github.com>
2020-05-03 14:51:42 +0800

    used vpc v0.0.2

--- terraform/projects/myvpn/vpc-module.tf
+++ terraform/projects/myvpn/vpc-module.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git@github.com:teamulap/terraform-aws-module-vpc.git//terraform/modules?ref=v0.0.1"
+    source = "git@github.com:teamulap/terraform-aws-module-vpc.git//terraform/modules?ref=v0.0.2"
     region                       = "ap-northeast-1"
     vpc_cidr                     = "10.0.0.0/16"
     vpc_subnets                  = ["10.0.0.0/24","10.0.1.0/24","10.0.2.0/24"]
